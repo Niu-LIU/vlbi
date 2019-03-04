3c39ffdd2cddbe2e8c2a2ec4e9918f6cd68bffbd
liuniu@smail.nju.edu.com <Neo@SUZAKU-2.local>
2019-03-04 21:35:14 +0800

    Rewrite read_vel.py

--- read_vel.py
+++ read_vel.py
@@ -47,126 +47,219 @@ velocities are also written.
    89-96   F8.3   mm/yr formal uncertainty of N-component of station velocity.
 """
 
+
+from astropy.table import Table, join
+from astropy import units as u
 import numpy as np
+import os
 import sys
 
 
+__all__ = ["read_vel"]
+
+
 # ------------------------------  FUNCTIONS  ---------------------------
-def read_vel(dataflea):
-    '''Retrieve the result from .lso file.
+def read_sta_gvx(gvx_file):
+    '''Retrieve cartesian components of station velocities.
 
     Parameters
     ----------
-    datafile : string
+    gvx_file : string
         name of data file
 
     Returns
     ----------
-    staname : string
-        name of station
-    VX : array, float
-        Velocity of X component, mm/yr
-    VY : array, float
-        Velocity of Y component, mm/yr
-    VZ : array, float
-        Velocity of Z component, mm/yr
-    VX_err : array, float
-        formal uncertainty of VX component, mm/yr
-    VY_err : array, float
-        formal uncertainty of VY component, mm/yr
-    VZ_err : array, float
-        formal uncertainty of VZ component, mm/yr
-    VU : array, float
-        Velocity of U component, mm/yr
-    VE : array, float
-        Velocity of E component, mm/yr
-    VN : array, float
-        Velocity of N component, mm/yr
-    VU_err : array, float
-        formal uncertainty of VU component, mm/yr
-    VE_err : array, float
-        formal uncertainty of VE component, mm/yr
-    VN_err : array, float
-        formal uncertainty of VN component, mm/yr
+    sta_gvx : astropy.table object
+     |__
+        station : string
+            name of station
+        xv : array, float
+            X component of station velocity
+        yv : array, float
+            Y component of station velocity
+        zv : array, float
+            Z component of station velocity
+        xv_err : array, float
+            formal uncertainty of X component of station velocity
+        yv_err : array, float
+            formal uncertainty of Y component of station velocity
+        zv_err : array, float
+            formal uncertainty of Z component of station velocity
     '''
 
-    # empty list for store data
-    staname = []
-    # XYZ
-    VX = []
-    VX_err = []
-    VY = []
-    VY_err = []
-    VZ = []
-    VZ_err = []
-    # UEN
-    VU = []
-    VU_err = []
-    VE = []
-    VE_err = []
-    VN = []
-    VN_err = []
+    # Check if the input file exists
+    if not os.path.exists(gvx_file):
+        print("Couldn't find the input file", gvx_file)
+        sys.exit()
 
-    for line in open(datafile, 'r'):
-        if line[0] != '#':
-            if line[:7] == 'STA_GVX':
-                staname.append(line[10:18].rstrip())
-                VX.append(float(line[23:32]))
-                VX_err.append(float(line[36:44]))
-                VY.append(float(line[49:58]))
-                VY_err.append(float(line[62:70]))
-                VZ.append(float(line[75:84]))
-                VZ_err.append(float(line[88:96]))
-            elif line[:7] == 'STA_GVU':
-                VU.append(float(line[23:32]))
-                VU_err.append(float(line[36:44]))
-                VE.append(float(line[49:58]))
-                VE_err.append(float(line[62:70]))
-                VN.append(float(line[75:84]))
-                VN_err.append(float(line[88:96]))
-            else:
-                print("Something must be wrong! Please check your file")
-                exit()
+    sta_gvx = Table.read(gvx_file,
+                         format="ascii.fixed_width_no_header",
+                         names=["station", "xv", "xv_err", "yv", "yv_err",
+                                "zv", "zv_err"],
+                         col_starts=[10, 23, 36, 49, 62, 75, 88],
+                         col_ends=[18, 32, 44, 58, 70, 84, 96])
 
-    # List -> array, a rather stupid way.
-    staname = np.array(staname)
-    # XYZ
-    VX = np.array(VX)
-    VX_err = np.array(VX_err)
-    VY = np.array(VY)
-    VY_err = np.array(VY_err)
-    VZ = np.array(VZ)
-    VZ_err = np.array(VZ_err)
-    # UEN
-    VU = np.array(VU)
-    VU_err = np.array(VU_err)
-    VE = np.array(VE)
-    VE_err = np.array(VE_err)
-    VN = np.array(VN)
-    VN_err = np.array(VN_err)
+    # Add information for units
+    sta_gvx["xv"].unit = u.m / 1000 / u.yr
+    sta_gvx["yv"].unit = u.m / 1000 / u.yr
+    sta_gvx["zv"].unit = u.m / 1000 / u.yr
+    sta_gvx["xv_err"].unit = u.m / 1000 / u.yr
+    sta_gvx["yv_err"].unit = u.m / 1000 / u.yr
+    sta_gvx["zv_err"].unit = u.m / 1000 / u.yr
 
-    return [staname, VX, VX_err, VY, VY_err, VZ, VZ_err,
-            VU, VU_err, VE, VE_err, VN, VN_err]
+    return sta_gvx
 
 
-# # Retrieve estimates.
-# if len(sys.argv) == 1:
-#     datafile = 'result/test.vel'
-# else:
-#     datafile = sys.argv[1]
-# [staname, VX, VX_err, VY, VY_err, VZ, VZ_err,
-#  VU, VU_err, VE, VE_err, VN, VN_err] = read_vel(datafile)
-# print(staname[0],
-#       VX[0],
-#       VX_err[0],
-#       VY[0],
-#       VY_err[0],
-#       VZ[0],
-#       VZ_err[0],
-#       VU[0],
-#       VU_err[0],
-#       VE[0],
-#       VE_err[0],
-#       VN[0],
-#       VN_err[0])
+def read_sta_gvu(gvu_file):
+    '''Retrieve local topocentric components of station velocities.
+
+    Parameters
+    ----------
+    gvu_file : string
+        name of data file
+
+    Returns
+    ----------
+    sta_gvu : astropy.table object
+     |__
+        station : string
+            name of station
+        uv : array, float
+            U component of station velocity
+        ev : array, float
+            E component of station velocity
+        nv : array, float
+            N component of station velocity
+        uv_err : array, float
+            formal uncertainty of U component of station velocity
+        ev_err : array, float
+            formal uncertainty of E component of station velocity
+        nv_err : array, float
+            formal uncertainty of N component of station velocity
+    '''
+
+    # Check if the input file exists
+    if not os.path.exists(gvu_file):
+        print("Couldn't find the input file", gvu_file)
+        sys.exit()
+
+    sta_gvu = Table.read(gvu_file,
+                         format="ascii.fixed_width_no_header",
+                         names=["station", "uv", "uv_err", "ev", "ev_err",
+                                "nv", "nv_err"],
+                         col_starts=[10, 23, 36, 49, 62, 75, 88],
+                         col_ends=[18, 32, 44, 58, 70, 84, 96])
+
+    # Add information for units
+    sta_gvu["uv"].unit = u.m / 1000 / u.yr
+    sta_gvu["ev"].unit = u.m / 1000 / u.yr
+    sta_gvu["nv"].unit = u.m / 1000 / u.yr
+    sta_gvu["uv_err"].unit = u.m / 1000 / u.yr
+    sta_gvu["ev_err"].unit = u.m / 1000 / u.yr
+    sta_gvu["nv_err"].unit = u.m / 1000 / u.yr
+
+    return sta_gvu
+
+
+def parse_vel_file(in_file, out_file="temp.out", keyword=None):
+    '''printer the lines containing keyword in .vel file
+
+    Parameters
+    ----------
+    in_file : string
+        .sta file
+    out_file : string
+        output file, default is None
+    keyword : string
+        keyword for grep. It could be set as "STA_GVU" or "STA_GVX".
+    '''
+
+    keyword_list = ["STA_GVX", "STA_GVU"]
+
+    if keyword is None or not keyword in keyword_list:
+        print("Valid keyword!")
+        sys.exit()
+
+    # Check if the input file exists
+    if not os.path.exists(in_file):
+        print("Couldn't find the input file", in_file)
+        sys.exit()
+
+    # Assume that the operation system is an Unix- or Linux-like where
+    # the commond grep is supported.
+    print("Generate a temporary file", out_file, "of type", keyword)
+    os.system("grep %s %s > %s" % (keyword, in_file, out_file))
+    print("Done!")
+
+    # Read the data
+    print("Read", out_file)
+    if keyword is "STA_GVX":
+        data_table = read_sta_gvx(out_file)
+    else:
+        data_table = read_sta_gvu(out_file)
+    print("Done!")
+
+    # Delete the temporary file
+    print("Delete", out_file)
+    os.system("rm %s" % out_file)
+    print("Done!")
+
+    return data_table
+
+
+def read_vel(vel_file):
+    '''Retrieve the result from .lso file.
+
+    Parameters
+    ----------
+    vel_file : string
+        name of .vel file
+
+    Returns
+    ----------
+    sta_vel : astropy.table object
+     |__
+        station : string
+            name of station
+        xv : array, float
+            X component of station velocity
+        yv : array, float
+            Y component of station velocity
+        zv : array, float
+            Z component of station velocity
+        xv_err : array, float
+            formal uncertainty of X component of station velocity
+        yv_err : array, float
+            formal uncertainty of Y component of station velocity
+        zv_err : array, float
+            formal uncertainty of Z component of station velocity
+        uv : array, float
+            U component of station velocity
+        ev : array, float
+            E component of station velocity
+        nv : array, float
+            N component of station velocity
+        uv_err : array, float
+            formal uncertainty of U component of station velocity
+        ev_err : array, float
+            formal uncertainty of E component of station velocity
+        nv_err : array, float
+            formal uncertainty of N component of station velocity
+    '''
+
+    # Original data contains 2 types of data, which will be read repsectively
+    # 1) STA_GVX (Cartesian components of the vector of station velocities)
+    sta_gvx = parse_vel_file(vel_file, keyword="STA_GVX")
+
+    # 2) STA_GVU (Local topocentric components of station velocities)
+    sta_gvu = parse_vel_file(vel_file, keyword="STA_GVU")
+
+    # Merge three tables into one major table
+    sta_vel = join(sta_gvx, sta_gvu, keys="station", join_type="outer")
+
+    return sta_vel
+
+
+if __name__ == '__main__':
+    print("Nothing to do!")
 # ------------------------------ END -----------------------------------
